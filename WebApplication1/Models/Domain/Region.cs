20dcaec54ca8f127e03eb0fc2fddf79dc7e4aba3
Samin Ghoddosi <saminghoddosi44@gmail.com>
2025-10-03 22:09:28 -0300

    first commit

---
+++ WebApplication1/Models/Domain/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public int Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
